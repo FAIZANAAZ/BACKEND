21a87795b78055390f9f22fc1846e050cee5c9ee
FAIZANAAZ779 <faixanaaz@gmail.com>
2025-09-12 20:06:46 +0500

    unicode

---
+++ NODEJS/online_class_code/buffer/buf.js
@@ -0,0 +1,18 @@
+const buf1=Buffer.from("hello")
+
+console.log(buf1)
+
+//ye terminal me and ESA dega <Buffer 48 6c 8b>
+
+///
+const text="hello"
+console.log(text.codePointAt(0))
+//codepointat value ka uni-code dikhata he Unicode Yani decimal me and ayga 
+
+condecimal=text.codePointAt(0)
+const conbinary=condecimal.toString
+console.log(conbinary)
+
+//string me dalny Sy wo isko binary me change krdega kioky Unicode ka string decimat to binary code hota he
+
+//ye osi Unicode ki string form dikha deg
